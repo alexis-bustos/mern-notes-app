bb455330bebd66ca8dd5993d5593a45d908dc0da
Alexis Bustos <alexisbustos10@yahoo.com>
2025-09-26 21:27:53 -0700

    feat: add auth routes and protect pages with PrivateRoute in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,37 @@ import CreatePage from "./pages/CreatePage";
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
-import toast from "react-hot-toast";
+// import toast from "react-hot-toast";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import PrivateRoute from "./components/PrivateRoute";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="relative h-full w-full">
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreatePage />} />
-        <Route path="/note/:id" element={<NoteDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route
+          path="/create"
+          element={
+            <PrivateRoute>
+              <CreatePage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/note/:id"
+          element={
+            <PrivateRoute>
+              <NoteDetailPage />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </div>
   );
